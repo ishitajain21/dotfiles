a35edbb2dd0f04de78116c404dc94515ad3ceeb8
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2025-01-18 00:16:56 -0800

    Update dotfiles: 2025-01-18-0016

---
+++ vscode/install_extensions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script to install VSCode extensions
+code --install-extension GitHub.remotehub
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension KevinRose.vsc-python-indent
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mikoz.black-py
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.flake8
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.azure-repos
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension ms-vscode.remote-explorer
+code --install-extension ms-vscode.remote-repositories
+code --install-extension nwgh.bandit
+code --install-extension oderwat.indent-rainbow
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vscodevim.vim
+code --install-extension Vue.volar
+code --install-extension Vue.vscode-typescript-vue-plugin
+code --install-extension willemmirkovich.jupyter-vim-keymap
